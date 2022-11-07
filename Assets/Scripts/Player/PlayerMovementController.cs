2f5305ced28881411d7350be2df0bd4e6836e652
William Boudreault <boudreaultwilliam@gmail.com>
2022-11-06 21:57:32 -0500

    Ajout de gravité pour le joueur

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovementController : MonoBehaviour
 {
+    [Header("Speeds")]
     [SerializeField] private float speed = 10f;
+    [SerializeField] private float gravity = -10f;
+    [SerializeField] private float decelaration = 0.95f;
 
+    [Header("Ground Checking")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.3f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Vector3 velocity = Vector3.zero;
+    private bool isOnGround = false;
     private CharacterController controller;
 
     private void Start()
@@ -16,7 +26,17 @@ public class PlayerMovementController : MonoBehaviour
 
     private void Update()
     {
+        isOnGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
         GetInputs();
+
+        if (isOnGround && velocity.y < 0)
+            velocity.y = -1f;
+        
+        CalcGravity();
+
+        Move();
+
+        velocity *= decelaration * Time.deltaTime;
     }
 
     private void GetInputs()
@@ -25,7 +45,18 @@ public class PlayerMovementController : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
+        move *= speed * Time.deltaTime;
 
-        controller.Move(move * speed * Time.deltaTime);
+        velocity += move;
+    }
+
+    private void CalcGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+    }
+
+    private void Move()
+    {
+        controller.Move(velocity);
     }
 }
